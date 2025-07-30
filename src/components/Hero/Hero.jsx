f922acf9417c3fc609774895e459e94da2a89849
KalelBunito <kalelhbarboza@gmail.com>
2025-07-30 18:18:00 -0300

    atualizaçao menu

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,31 @@
 // importar estilo, outros componentes e etc...
-import styles from './Hero.module.css'
+import styles from './Hero.module.css';
+import React, { useState } from 'react';
+
 
 export default function Hero() {
     // Lógica do JAVASCRIPT
+    const [isModalOpen, setIsModalOpen] = useState(false)
     return (
         <main className={styles.heroContainer}>
             <div className={styles.heroLeft}>
-                <h1>Kalel <br></br> Hauck Barboza </h1>
+                <h1>Kalel <br></br> Hauck</h1>
                 <h2>Desenvolvedor FullStack</h2>
                 <span className={styles.containerButtons}>
-                    <button>Fale Comigo</button>
+                    <button onClick={() => setIsModalOpen(true)}>Fale Comigo</button>
                     <button>Baixar meu currículo</button>
                 </span>
             </div>
             <div className={styles.heroRight}>
                 <img src='rena.webp' alt='imagem seçao hero' />
             </div>
+
+            {isModalOpen && (
+                <div>
+                    <p>Fui clicado</p>
+                </div>
+            )}
+
         </main>
     )
 }
