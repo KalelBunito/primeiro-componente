b4ed3e55f6a82cda5be192b5d2e784292af77401
KalelBunito <kalelhbarboza@gmail.com>
2025-08-06 18:51:10 -0300

    adiçao imagens

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Skills from './components/skills/Skills.jsx'
+import Projects from './components/Projects/Projects.jsx'
 
 export default function App() {
  
@@ -12,6 +13,7 @@ export default function App() {
         <Navbar />
         <Hero />
         <Skills />
+        <Projects />
       </div>
   )
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,20 @@
+import { MdCatchingPokemon } from "react-icons/md";
+import { BsNintendoSwitch } from "react-icons/bs";
+import { CgPacman } from "react-icons/cg";
+import { FaHtml5 } from "react-icons/fa";
+import {motion} from 'framer-motion';
+
+export default function Projects(){
+
+    const projects = [
+        {icon: <CgPacman />,title: 'PacMan Game',desc: '',demo: '', },
+        {icon: <BsNintendoSwitch />,title: 'Mrio Run Game',desc: '',img: '',demo: ''},
+        {icon: <MdCatchingPokemon />,title: 'Pokedéx',desc: '',img: '',demo: ''},
+        {icon: <FaHtml5 />,title: 'Formulário-API',desc: '',img: '',demo: ''}
+    ]
+    return(
+        <section>
+            
+        </section>
+    )
+}
